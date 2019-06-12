7ee881579b50d7707a6abf1ba6260f0a1512d4b0
Krishna Srinivasmurthy <krishna.p.srinivasmurthy@boeing.com>
2019-06-12 14:08:11 -0700

    increased stopwords

--- src/main/java/com/hcl/controllers/UploadController.java
+++ src/main/java/com/hcl/controllers/UploadController.java
@@ -9,6 +9,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletResponse;
@@ -38,53 +39,64 @@ import com.kennycason.kumo.palette.LinearGradientColorPalette;
  */
 @RestController
 public class UploadController {
+	private static Set<String> stopWords = null;
+	static {
+		try {
+			stopWords = loadStopWords();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	@RequestMapping(value = "/uploadResume", method = RequestMethod.POST)
 	@ResponseBody
 	public void uploadResume(@RequestParam("file") MultipartFile file, HttpServletResponse response) throws Exception {
-		String extractedText = "";
+		if (stopWords != null) {
+			String extractedText = "";
 
-		if (file.getOriginalFilename().endsWith("doc")) {
-			WordExtractor ex = new WordExtractor(file.getInputStream());
-			extractedText = ex.getText();
-			ex.close();
-		} else if (file.getOriginalFilename().endsWith("docx")) {
-			XWPFDocument doc = new XWPFDocument(file.getInputStream());
-			XWPFWordExtractor extractor = new XWPFWordExtractor(doc);
-			extractedText = extractor.getText();
-			extractor.close();
-		} else if (file.getOriginalFilename().endsWith("pdf")) {
-			PDFTextStripper pdfStripper = new PDFTextStripper();
-			try (PDDocument pdDoc = PDDocument.load(file.getInputStream())) {
-				extractedText = pdfStripper.getText(pdDoc);
+			if (file.getOriginalFilename().endsWith("doc")) {
+				WordExtractor ex = new WordExtractor(file.getInputStream());
+				extractedText = ex.getText();
+				ex.close();
+			} else if (file.getOriginalFilename().endsWith("docx")) {
+				XWPFDocument doc = new XWPFDocument(file.getInputStream());
+				XWPFWordExtractor extractor = new XWPFWordExtractor(doc);
+				extractedText = extractor.getText();
+				extractor.close();
+			} else if (file.getOriginalFilename().endsWith("pdf")) {
+				PDFTextStripper pdfStripper = new PDFTextStripper();
+				try (PDDocument pdDoc = PDDocument.load(file.getInputStream())) {
+					extractedText = pdfStripper.getText(pdDoc);
+				}
+			} else {
+				throw new Exception(file.getOriginalFilename() + " format not supported yet");
 			}
-		} else {
-			throw new Exception(file.getOriginalFilename() + " format not supported yet");
-		}
 
-		final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
-		frequencyAnalyzer.setStopWords(loadStopWords());
-		frequencyAnalyzer.setWordFrequenciesToReturn(50);
-		frequencyAnalyzer.setMinWordLength(4);
-		System.out.println("***!!!!!" + extractedText);
-		final List<WordFrequency> wordFrequencies = frequencyAnalyzer
-				.load(new ByteArrayInputStream(extractedText.getBytes()));
-		final Dimension dimension = new Dimension(600, 600);
-		final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
-		wordCloud.setPadding(2);
-		wordCloud.setBackground(new RectangleBackground(dimension));
-		// colors followed by and steps between
-		wordCloud.setColorPalette(new LinearGradientColorPalette(Color.RED, Color.BLUE, Color.GREEN, 30, 30));
-		wordCloud.setFontScalar(new SqrtFontScalar(10, 40));
-		wordCloud.build(wordFrequencies);
-		wordCloud.writeToStream("png", response.getOutputStream());
+			final FrequencyAnalyzer frequencyAnalyzer = new FrequencyAnalyzer();
+			frequencyAnalyzer.setStopWords(stopWords);
+			frequencyAnalyzer.setWordFrequenciesToReturn(50);
+			frequencyAnalyzer.setMinWordLength(4);
+			System.out.println("***!!!!!" + extractedText);
+			final List<WordFrequency> wordFrequencies = frequencyAnalyzer
+					.load(new ByteArrayInputStream(extractedText.getBytes()));
+			final Dimension dimension = new Dimension(600, 600);
+			final WordCloud wordCloud = new WordCloud(dimension, CollisionMode.PIXEL_PERFECT);
+			wordCloud.setPadding(2);
+			wordCloud.setBackground(new RectangleBackground(dimension));
+			// colors followed by and steps between
+			wordCloud.setColorPalette(new LinearGradientColorPalette(Color.RED, Color.BLUE, Color.GREEN, 30, 30));
+			wordCloud.setFontScalar(new SqrtFontScalar(10, 40));
+			wordCloud.build(wordFrequencies);
+			wordCloud.writeToStream("png", response.getOutputStream());
+		}
 	}
 
-	private List<String> loadStopWords() throws IOException {
+	private static Set<String> loadStopWords() throws IOException {
 		// TODO Auto-generated method stub
 		try (InputStream resource = UploadController.class.getResourceAsStream("/stop-words.txt")) {
-			List<String> doc = new BufferedReader(new InputStreamReader(resource, StandardCharsets.UTF_8)).lines()
-					.collect(Collectors.toList());
+			Set<String> doc = new BufferedReader(new InputStreamReader(resource, StandardCharsets.UTF_8)).lines()
+					.collect(Collectors.toSet());
 			return doc;
 		}
 	}
